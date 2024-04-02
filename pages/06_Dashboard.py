db3433ce12d3421df9a9c2c47e4ae905c91dbca9
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2024-04-02 14:12:27 -0300

    Update 06_Dashboard.py

--- pages/06_Dashboard.py
+++ pages/06_Dashboard.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Dashboard - Passos Mágicos", layout="wide")
-
+st.set_page_config(page_title="Dashboard - Passos Mágicos")
+ 
 # Título da página
 st.title("Dashboard de Análise de Dados - Passos Mágicos")
 
