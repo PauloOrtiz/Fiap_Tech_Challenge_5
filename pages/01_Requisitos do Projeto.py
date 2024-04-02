12c45a745a9722bb49a0b7301df17add222fee39
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2024-04-02 13:12:02 -0300

    da

--- pages/01_Requisitos do Projeto.py
+++ pages/01_Requisitos do Projeto.py
@@ -3,8 +3,10 @@ import streamlit as st
 # Definindo o layout da página
 st.set_page_config(page_title="Passos Mágicos - Transformação Digital")
 
+with open("./src/css/style.css") as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+
 # Título
 st.title("Bem-vindo à Transformação Digital de 'Passos Mágicos'")
 
-with open("./src/css/style.css") as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
