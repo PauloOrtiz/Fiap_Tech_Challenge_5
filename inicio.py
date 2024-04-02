72b1964d1380e87ace7541f2aee9b5a46413acbe
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2024-04-02 13:14:01 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,28 +1,40 @@
 import streamlit as st
 
 # Definindo o layout da página
-st.set_page_config(page_title="Passos Mágicos - Transformação Digital")
-
+st.set_page_config(page_title="Introdução e Objetivos - Passos Mágicos")
 with open("./src/css/style.css") as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Título
-st.title("Bem-vindo à Transformação Digital de 'Passos Mágicos'")
+st.title("Introdução e Objetivos do Projeto Passos Mágicos")
 
-
-
-# Introdução com Storytelling
+# Seção de Introdução
+st.header("Introdução")
 st.write("""
-Imagine uma organização onde cada passo conta, onde cada ação cria um eco de mudança e esperança. 
-"Passos Mágicos" é essa organização, uma entidade dedicada a iluminar vidas e construir futuros melhores 
-através de suas causas sociais. Este projeto, uma colaboração entusiasmada em uma datathon, é uma jornada 
-para levar "Passos Mágicos" para o próximo patamar na era digital.
+O projeto 'Passos Mágicos' representa uma iniciativa inovadora de transformação digital para uma organização dedicada a causas sociais significativas. Surgiu da necessidade de modernizar os processos de coleta, gestão e análise de dados, buscando uma maior eficiência e impacto nas ações da organização.
+""")
 
-Neste espaço digital, você embarcará em uma viagem de inovação e impacto. Vamos guiar você através de como, 
-com a ajuda das ferramentas da Microsoft – Forms, Power Automate, SharePoint e Power BI – transformamos dados 
-dispersos em histórias de sucesso, ideias em ações, e desafios em soluções. Cada formulário preenchido, cada 
-dado processado, e cada insight gerado no Power BI, não é apenas um número; é uma vida, um sonho, um passo em 
-direção a um futuro brilhante.
+# Seção de Objetivos
+st.header("Objetivos do Projeto")
+st.write("""
+Os principais objetivos do projeto incluem:
+- **Automatização de Formulários:** Utilizar Microsoft Forms para otimizar a coleta de dados.
+- **Integração e Automatização de Processos:** Implementar o Power Automate para assegurar uma transferência eficiente de dados para o SharePoint.
+- **Melhoria na Gestão de Dados:** Aproveitar as capacidades do SharePoint para armazenar e gerenciar dados de forma centralizada e segura.
+- **Análise Avançada de Dados:** Utilizar o Power BI para transformar dados coletados em insights valiosos e relatórios interativos.
+- **Impacto Social:** Contribuir para a eficácia e eficiência das decisões e ações da 'Passos Mágicos', potencializando seu impacto social.
+""")
+
+# Seção de Contexto
+st.header("Contexto de Desenvolvimento")
+st.write("""
+Este projeto nasceu de uma datathon, onde o desafio era utilizar tecnologia para solucionar problemas reais enfrentados por organizações sem fins lucrativos. A 'Passos Mágicos' foi escolhida como beneficiária desta iniciativa, dada a sua importante missão social e a necessidade evidente de uma transformação digital.
+""")
+
+# Seção de Expectativas
+st.header("Expectativas para o Futuro")
+st.write("""
+Esperamos que este projeto não apenas melhore os processos internos da 'Passos Mágicos', mas também inspire outras organizações a adotarem tecnologias digitais para ampliar seu impacto social. Acreditamos que a transformação digital é uma poderosa alavanca para o avanço social e estamos entusiasmados para ver as mudanças positivas que ela trará.
 """)
 
 # Rodapé
